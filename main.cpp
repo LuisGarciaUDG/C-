0ba0a73bc10a941094f911d864dfe3c2472d3a3b
Luis Alberto García Rodríguez <luis.gcdz@gmail.com>
2018-04-15 17:08:32 -0500

    Primer repositorio creado y supervisado con git bash

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/*
+Programa número 2: suma de dos números enteros
+Estructura: secuencial
+Autor: Luis Alberto García Rodríguez
+N° lista: 11
+Fecha: 3 de frebrero del 2015
+*/
+#include <iostream>// Programa suma
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+int a,b,c;
+cout<<"\tLa suma";
+cout<<endl<<endl<<endl;
+cout<<"Dame el valor de A: ";
+cin>>a;
+cout<<endl;
+cout<<"Dame el valor de B: ";
+cin>>b;
+cout<<endl<<endl<<endl;
+c=a+b;
+cout<<"La suma es:         ";
+cout<<c;
+cout<<endl<<endl<<endl<<endl;
+cout<<"Presione <enter> para salir";
+
+return 0;
+}
